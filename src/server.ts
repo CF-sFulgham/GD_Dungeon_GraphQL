ce074431ec45265c57604a1016dc9d42de14424e
steven fulgham <stevenfulgham@stevens-Mac-mini.local>
2025-05-15 18:06:07 +0900

    Adding configuration for GraphQL and GraphIQL.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
 import express from 'express';
 import { SERVER_PORT } from './constants';
+import schema from './typedefs';
+import root from './resolvers';
+import { graphqlHTTP } from 'express-graphql';
 
-const app = express();
+(async () => {
+    const app = express();
+    
+    app.use(
+        "/graphql",
+        express.json(),
+        graphqlHTTP({
+            schema,
+            rootValue: root,
+            graphiql: true
+        })
+    )
+
+    // Start the server
+    app.listen(SERVER_PORT, () => {
+        console.log(`Server is running on http://localhost:${SERVER_PORT}/graphql`);
+    });
+
+})().catch((err) => {
+    console.error('Error initializing Apollo Server:', err);
+});
 
 // Middleware
-app.use(express.json());
-
-// Routes
-app.get('/', (_, res) => {
-    res.send('Hello, World!');
-});
-
-// Start the server
-app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
-});
